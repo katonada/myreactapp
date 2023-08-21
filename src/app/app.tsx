d104c7bf03ea72398a39b3aa3c211d3928922c0c
katonada <davor.katona.sesto@wm.rs>
2023-08-21 06:53:22 +0200

    test nx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,19 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
+import {Route, Routes} from 'react-router-dom';
 
-import NxWelcome from './nx-welcome';
+// importing the component from the library
+import {ProductList} from 'modules/products';
+
+function Home() {
+    return <h1>Home</h1>;
+}
 
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="myreactapp" />
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Home/>}></Route>
+            <Route path="/products" element={<ProductList/>}></Route>
+        </Routes>
+    );
 }
 
 export default App;
